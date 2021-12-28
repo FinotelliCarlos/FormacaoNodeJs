99c089eaf5c1d249a731c3e4d91b1406f81dcebd
Finotelli <carlos.finotelli@outlook.com>
2021-12-27 22:37:57 -0300

    Update app.js

--- init/app.js
+++ init/app.js
@@ -3,5 +3,6 @@ const site = 'https://finotelli-me.vercel.app/';
 
 if(exibirSite){
     console.log("Seja bem vindo a minha Formação em Node.Js")
-    console.log("Me chamo Carlos e você pode me conhecer mais em : " + site)
+    console.log("Me chamo Carlos")
+    console.log("Você pode me conhecer mais em : " + site)
 }
